087d1fe66d8e0b34e767dab3249bcf1548629c51
Grabacr07 <grabacr07+github@gmail.com>
2016-12-22 13:44:56 +0900

    Add sample project.

---
+++ src/AvalonShell/UI/Window.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AvalonShell.UI
+{
+	partial class Window
+	{
+		public Window()
+		{
+			var host = new PowerShellHost();
+
+			this.InitializeComponent();
+			this.Console.PowerShellHost = host;
+
+			host.Open();
+		}
+	}
+}
